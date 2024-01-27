5d29aadc3421f870d0a6c02e9694b3c666e6aab5
leomeden <leonardomeden@gmail.com>
2024-01-27 15:10:39 -0300

    added main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+let btnEncriptar = document.getElementsByClassName("btn-encriptar")[0];
+let btnDesencriptar = document.getElementsByClassName("btn-desencriptar")[0];
+let btnCopiar = document.getElementsByClassName("btn-copiar")[0];
+
+btnEncriptar.addEventListener("click", ()=> console.log("Apretado botón Encriptar"))
+btnDesencriptar.addEventListener("click", ()=> console.log("Apretado botón Desencriptar"))
+btnCopiar.addEventListener("click", ()=> console.log("Apretado botón Copiar"))
